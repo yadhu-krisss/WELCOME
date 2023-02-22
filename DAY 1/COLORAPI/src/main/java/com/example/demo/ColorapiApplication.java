acb86e7afd7cf212c067c98eeb9b9078198a0593
yadhu-krisss <119097296+yadhu-krisss@users.noreply.github.com>
2023-02-22 15:09:43 +0530

    Add files via upload

---
+++ DAY 1/COLORAPI/src/main/java/com/example/demo/ColorapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorapiApplication.class, args);
+	}
+
+}
